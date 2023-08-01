c81bfe199c10af9e5daca3a131ba153dceadcd12
rorense <rkorense@gmail.com>
2023-08-01 18:06:57 +1200

    Bug fixes

--- fb_clone/src/context_api/StateProvider.js
+++ fb_clone/src/context_api/StateProvider.js
@@ -1,6 +1,7 @@
 // Importing hooks
 import React, { createContext, useContext, useReducer} from "react";
 
+// Creating context that will hold the data being passed around AKA user info
 export const StateContext = createContext();
 
 // Higher order component
